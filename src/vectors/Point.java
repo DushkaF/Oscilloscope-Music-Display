4ede02410260ff929730ec7f80bd9054276d0a36
Vorazen <alexander.atiskov@yandex.ru>
2022-02-08 20:17:52 +0300

    added Point and changed Vector classes

---
+++ src/vectors/Point.java
@@ -0,0 +1,49 @@
+package vectors;
+
+import java.util.Objects;
+
+public class Point {
+    private short x;
+    private short y;
+
+    public Point(short x,short y){
+        this.x=x;
+        this.y=y;
+    }
+    public Point(int x,int  y){
+        this.x= (short) x;
+        this.y= (short) y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public short getX() {
+        return x;
+    }
+
+    public void setX(short x) {
+        this.x = x;
+    }
+
+    public short getY() {
+        return y;
+    }
+
+    public void setY(short y) {
+        this.y = y;
+    }
+}
--- src/vectors/Vector.java
+++ src/vectors/Vector.java
@@ -1,4 +1,69 @@
 package vectors;
 
+import java.util.Objects;
+
 public class Vector {
+    private Point start;
+    private Point end;
+    private short length;
+
+    public Vector(short xS, short yS,short xE, short yE){
+        start=new Point(xS,yS);
+        end=new Point(xS,yS);
+        setLength();
+    }
+
+    public Vector(int xS, int yS, int xE, int yE){
+        start=new Point(xS,yS);
+        end=new Point(xS,yS);
+        setLength();
+    }
+    public Vector(Point s, Point e){
+        start=s;
+        end=e;
+        setLength();
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start = start;
+        setLength();
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+        setLength();
+    }
+
+    public short getLength() {
+        return length;
+    }
+
+    private short setLength(){
+        return (short) Math.sqrt(Math.pow(start.getX()-end.getX(),2)+Math.pow(start.getY()-end.getY(),2));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return Objects.equals(start, vector.start) && Objects.equals(end, vector.end);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "start=" + start +
+                ", end=" + end +
+                ", length=" + length +
+                '}';
+    }
 }
