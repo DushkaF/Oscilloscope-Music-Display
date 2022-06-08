e43f9774e81cd13fd4dc0b68ed2af93f286a7a2c
Dushko Fedor <fdsaq1234@ya.ru>
2022-06-08 20:56:41 +0300

    I am make a marry

---
+++ src/io/output/PlayThread.java
@@ -0,0 +1,32 @@
+package output;
+
+import java.io.*;
+import javax.sound.sampled.*;
+
+public class PlayThread extends Thread {
+    SourceDataLine sourceDataLine;
+    AudioInputStream audioInputStream;
+    PlayThread (SourceDataLine _sourceDataLine, AudioInputStream _audioInputStream) {
+        sourceDataLine = _sourceDataLine;
+        audioInputStream = _audioInputStream;
+    }
+
+    byte tempBuffer[] = new byte[10000];
+
+    public void run() {
+        try {
+            int cnt;
+
+            while ((cnt = audioInputStream.read(tempBuffer, 0, tempBuffer.length)) != -1) {
+                if (cnt > 0) {
+                    sourceDataLine.write(tempBuffer, 0, cnt);
+                }
+            }
+            sourceDataLine.drain();
+            sourceDataLine.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+}
