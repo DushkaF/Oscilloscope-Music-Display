dfd3fc1983cd24d2db9069f7ab8d5e891e26c7eb
Dushko Fedor <fdsaq1234@ya.ru>
2022-06-09 22:25:43 +0300

    Make a loop sound and good image changing

--- src/io/output/Output.java
+++ src/io/output/Output.java
@@ -13,14 +13,24 @@ import java.util.LinkedList;
 
 public class Output implements Runnable {
     private OutputArgs outputArgs;
+    private AudioFormat audioFormat;
     private Thread outputThread;
+    private PlayThread playThread;
+    private  Clip clip;
 
     public Output(OutputArgs outputArgs) {
         this.outputArgs = outputArgs;
+        audioFormat = outputArgs.getAudioFormat();
+        try {
+            clip = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void draw(LinkedList<Vector> map, double kFactor) {
         //System.out.println("k " + kFactor);
+        LinkedList<int[]> outputReadyStream = new LinkedList<>();
         for (Vector nextVector : map) {
             if (nextVector.visible) {
                 int startX = (int) ((double) nextVector.start.x * kFactor - Math.pow(2, outputArgs.sampleSizeInBits) / 2.0);
@@ -32,11 +42,11 @@ public class Output implements Runnable {
                 Point convertedEnd = new Point(endX, endY, 0, 0);
 //                System.out.println("[" + convertedStart.toString() + " " + convertedEnd.toString() + "] " + nextVector.visible);
 
-                drawBresenhamLine(  convertedStart, convertedEnd);
+                drawBresenhamLine(convertedStart, convertedEnd, outputReadyStream);
             }
         }
 
-        createTone(1);
+        byteArrayOutputStream = createTone(outputReadyStream);
         playAudio();
     }
 
@@ -55,26 +65,22 @@ public class Output implements Runnable {
 
     }
 
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    AudioInputStream audioInputStream;
+    ByteArrayOutputStream byteArrayOutputStream ;
     SourceDataLine sourceDataLine;
-    LinkedList<int[]> outputReadyStream = new LinkedList<>();
 
 
-
-    private void createTone(float time) {
-        float rate = outputArgs.getAudioFormat().getSampleRate();
-        for (int t = 0; t < (rate * time / outputReadyStream.size()); t++) {
-            for (int[] nextPoint : outputReadyStream) {
-                byte[] buf = new byte[]{(byte) nextPoint[0], (byte) nextPoint[1]};
-                byteArrayOutputStream.write(buf, 0, 2);
-            }
+    private ByteArrayOutputStream createTone(LinkedList<int[]> outputReadyStream) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        for (int[] nextPoint : outputReadyStream) {
+            byte[] buf = new byte[]{(byte) nextPoint[0], (byte) nextPoint[1]};
+            outputStream.write(buf, 0, 2);
         }
         System.out.println("End form");
+        return outputStream;
     }
 
 
-    private void drawBresenhamLine(Point start, Point end) {
+    private void drawBresenhamLine(Point start, Point end, LinkedList<int[]> outputList) {
         int x, y, dx, dy, incx, incy, pdx, pdy, es, el, err;
 
         dx = end.x - start.x;
@@ -100,11 +106,10 @@ public class Output implements Runnable {
 
         x = start.x;
         y = start.y;
-        outputReadyStream.addLast(new int[]{x, y});
+        outputList.addLast(new int[]{x, y});
 
         err = el / 2;
-        for (int t = 0; t < el; t++)
-        {
+        for (int t = 0; t < el; t++) {
             err -= es;
             if (err < 0) {
                 err += el;
@@ -114,29 +119,33 @@ public class Output implements Runnable {
                 x += pdx;
                 y += pdy;
             }
-            outputReadyStream.addLast(new int[]{x, y});
+            outputList.addLast(new int[]{x, y});
         }
     }
 
 
     private void playAudio() {
         try {
-            byte audioData[] = byteArrayOutputStream.toByteArray();
+            byte[] audioData = byteArrayOutputStream.toByteArray();
             InputStream byteArrayInputStream = new ByteArrayInputStream(audioData);
-
-            AudioFormat audioFormat = outputArgs.getAudioFormat();
-            audioInputStream = new AudioInputStream(
+            AudioInputStream audioInputStream = new AudioInputStream(
                     byteArrayInputStream,
                     audioFormat,
                     audioData.length / audioFormat.getFrameSize());
 
-            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
-            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-            sourceDataLine.open(audioFormat);
-            sourceDataLine.start();
+//            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
+//            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+//            sourceDataLine.open(audioFormat);
+//            sourceDataLine.start();
+//            playThread = new PlayThread(audioFormat);
+//            Thread playThread = new PlayThread(audioFormat);
+//            playThread.start();
 
-            Thread playThread = new Thread(new PlayThread(sourceDataLine, audioInputStream));
-            playThread.start();
+            System.out.println(audioInputStream.getFormat().toString());
+            clip.stop();
+            clip.close();
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (Exception e) {
             System.out.println(e);
             System.exit(0);
--- src/io/output/PlayThread.java
+++ src/io/output/PlayThread.java
@@ -1,19 +1,29 @@
 package io.output;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.*;
 
 public class PlayThread extends Thread {
+    AudioFormat audioFormat;
     SourceDataLine sourceDataLine;
     AudioInputStream audioInputStream;
-    PlayThread(SourceDataLine _sourceDataLine, AudioInputStream _audioInputStream) {
-        sourceDataLine = _sourceDataLine;
-        audioInputStream = _audioInputStream;
+//    PlayThread(SourceDataLine _sourceDataLine, ) {
+//        sourceDataLine = _sourceDataLine;
+//    }
+
+    PlayThread(AudioFormat _audioFormat) {
+        audioFormat = _audioFormat;
+//        audioInputStream = new AudioInputStream();
     }
+
     byte tempBuffer[] = new byte[10000];
+
+    public void setCaptureStream(AudioInputStream newStream){
+        audioInputStream = newStream;
+    }
 
     public void run() {
         try {
+            /*
             int cnt;
 
             while ((cnt = audioInputStream.read(tempBuffer, 0, tempBuffer.length)) != -1) {
@@ -23,6 +33,11 @@ public class PlayThread extends Thread {
             }
             sourceDataLine.drain();
             sourceDataLine.close();
+             */
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioFormat, audioInputStream.readAllBytes(), 0, audioInputStream.available());
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (Exception e) {
             System.out.println(e);
             System.exit(0);
